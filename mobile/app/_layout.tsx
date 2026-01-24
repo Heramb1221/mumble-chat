0509b288dafbb8773cac2197facce52733a46ebf
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2026-01-24 20:16:26 +0530

    Mobile and auth error 1

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,14 +1,45 @@
 import { Stack } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo'
 import "../global.css";
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import AuthSync from "@/components/AuthSync";
+import { StatusBar } from "expo-status-bar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://efd20e60483d7f7af2c5436b0a3202bf@o4510764612255744.ingest.de.sentry.io/4510764613632080',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 const queryClient = new QueryClient();
 
-export default function RootLayout() {
+export default Sentry.wrap(function RootLayout() {
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Stack screenOptions={{ headerShown: false }}/>
-    </QueryClientProvider>
+    <ClerkProvider tokenCache={tokenCache}>
+      <QueryClientProvider client={queryClient}>
+        <AuthSync />
+        <StatusBar style="light"/>
+        <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: "#121417"} }}>
+          <Stack.Screen name="(auth)" options={{ animation: "fade" }}/>
+          <Stack.Screen name="(tabs)" options={{ animation: "fade" }}/>
+        </Stack>
+      </QueryClientProvider>
+    </ClerkProvider>
   );
-}
+});
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,10 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#4FD1C5",
+          light: "#7EE6DC",
+          dark: "#2FA89D",
+          soft: "#BFF3EC",
+        },
+
+        surface: {
+          DEFAULT: "#121417",
+          light: "#1C1F24",
+          dark: "#0B0D10",
+          card: "#181B20",
+        },
+
+        foreground: "#F8FAFC",
+        "muted-foreground": "#9CA3AF",
+        "subtle-foreground": "#6B7280",
+
+        success: "#22C55E",
+        warning: "#FACC15",
+        danger: "#EF4444",
+      },
+    },
   },
   plugins: [],
-}
+};
